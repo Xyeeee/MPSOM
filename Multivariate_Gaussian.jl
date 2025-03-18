94b9dcf38588ad524eaa45ece06046805cddadb1
Xy <xyeeee77@gmail.com>
2025-03-18 17:00:04 +0100

    fixed typo

--- Multivariate_Gaussian.jl
+++ Multivariate_Gaussian.jl
@@ -7,6 +7,7 @@ mass = 1.0
 ω₀ = 1.0
 g₁ = 1.0
 g₂ = 1e-3
+N_th = 1.0
 
 
 
@@ -27,11 +28,11 @@ g(n) = n * g₁/sqrt(2 * mass * Ω(n))
 function get_A(n,m,t)
 
     A = zeros(Complex{Float64}, 8, 8)
-    A[1,1] = 2 + 1/(σ^2) + ν(n)/μ(n) + ν(m)/μ(m)
+    A[1,1] = 2 + 2/(N_th) + ν(n)/μ(n) + ν(m)/μ(m)
     A[1,2] = im * (ν(n)/μ(n) - ν(m)/μ(m))
     A[2,1] = im * (ν(n)/μ(n) - ν(m)/μ(m))
     A[1,3] = -1/μ(n)
-    A[4,1] = -1/μ(n)
+    A[3,1] = -1/μ(n)
     A[1,4] = im/μ(n)
     A[4,1] = im/μ(n)
     A[1,5] = 0
@@ -43,7 +44,7 @@ function get_A(n,m,t)
     A[1,8] = -im/μ(m)
     A[8,1] = -im/μ(m)
 
-    A[2,2] = 2 + 1/(σ^2) - ν(n)/μ(n) - ν(m)/μ(m)
+    A[2,2] = 2 + 2/(N_th) - ν(n)/μ(n) - ν(m)/μ(m)
     A[2,3] = -im/μ(n)
     A[3,2] = -im/μ(n)
     A[2,4] = -1/μ(n)
@@ -117,6 +118,13 @@ end
 
 C(n,m,t) = -1/2 * (abs(β(n,t))^2 + abs(β(m,t))^2 - ν(m)/μ(m) * conj(β(m,t)^2) - ν(n)/μ(n) * β(m,t)^2)
 
-int_total(n,m,t) = 1/(π^4*sqrt(2 * π * σ^2))*exp(-im * (ψ(n,t)-ψ(m,t)))/(μ(n) * μ(m)) * sqrt((2*π)^8/det(get_A(n,m,t))) * exp(C(n,m,t)) * exp(1/2 * get_B(n,m,t)' * get_A(n,m,t)^-1 * get_B(n,m,t))
+int_total(n,m,t) = 1/(π^3 * π * N_th)*exp(-im * (ψ(n,t)-ψ(m,t)))/(μ(n) * μ(m)) * sqrt((2*π)^8/det(get_A(n,m,t))) * exp(C(n,m,t)) * exp(1/2 * get_B(n,m,t)' * get_A(n,m,t)^-1 * get_B(n,m,t))
 
-int_total(2,3,1)
+n = 1
+m = 1
+t = 1
+println("A: ",sqrt((2)^8/det(get_A(n,m,t))))
+println("B: ", exp(1/2 * get_B(n,m,t)' * get_A(n,m,t)^-1 * get_B(n,m,t)))
+println("C: ", exp(C(n,m,t)))
+
+int_total(n,m,t)

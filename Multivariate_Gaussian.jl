3b80aa8743b003656efa949e46c76119b593c9cc
Xy <xyeeee77@gmail.com>
2025-04-16 15:12:06 +0200

    Added missing phase factor to get_B

--- Multivariate_Gaussian.jl
+++ Multivariate_Gaussian.jl
@@ -6,7 +6,7 @@ mass = 1.0
 σ = 1.0
 ω₀ = 1.0
 g₁ = 1.0
-g₂ = 1e-3
+g₂ = 1e-10
 N_th = 1.0
 
 
@@ -112,19 +112,39 @@ function get_B(n,m,t)
     B[6] = 2 * im/μ(n) * β(n,t) -2* im/μ(m) * conj(β(m,t))
     B[7] = 2 * (real(β(m,t)) * cos(Ω(m) * t) - imag(β(m,t)) * sin(Ω(m) * t) - conj(β(m,t)) *  ν(m)/μ(m) *  exp(im * Ω(m) * t))
     B[8] = 2 * (real(β(m,t)) * sin(Ω(m) * t) + imag(β(m,t)) * cos(Ω(m) * t) + im * conj(β(m,t)) *  ν(m)/μ(m) *  exp(im * Ω(m) * t))
-
+    
+    B[3] += - β(n,t) * exp(im * Ω(n) * t) + conj(β(n,t)) * exp(-im * Ω(n) * t)
+    B[4] += im * β(n,t) * exp(im * Ω(n) * t) + im * conj(β(n,t)) * exp(-im * Ω(n) * t)
+    B[7] += - conj(β(m,t)) * exp(-im * Ω(m) * t) + β(m,t) * exp(im * Ω(m) * t)
+    B[8] += - im * conj(β(m,t)) * exp(-im * Ω(m) * t) - im * β(m,t) * exp(im * Ω(m) * t)
     return -1/2 * B
 end
 
-C(n,m,t) = -1/2 * (abs(β(n,t))^2 + abs(β(m,t))^2 - ν(m)/μ(m) * conj(β(m,t)^2) - ν(n)/μ(n) * β(m,t)^2)
+C(n,m,t) = -1/2 * (abs(β(n,t))^2 + abs(β(m,t))^2 - ν(m)/μ(m) * conj(β(m,t)^2) - ν(n)/μ(n) * β(n,t)^2) 
+int_total(n,m,t) = 1/(π^3 * π * N_th)*exp(-im * (ψ(n,t)-ψ(m,t)))/(μ(n) * μ(m)) * sqrt((2*π)^8/det(get_A(n,m,t))) * exp(C(n,m,t)) * exp(1/2* get_B(n,m,t)' * get_A(n,m,t)^-1 * get_B(n,m,t))
 
-int_total(n,m,t) = 1/(π^3 * π * N_th)*exp(-im * (ψ(n,t)-ψ(m,t)))/(μ(n) * μ(m)) * sqrt((2*π)^8/det(get_A(n,m,t))) * exp(C(n,m,t)) * exp(1/2 * get_B(n,m,t)' * get_A(n,m,t)^-1 * get_B(n,m,t))
 
-n = 1
-m = 1
-t = 1
-println("A: ",sqrt((2)^8/det(get_A(n,m,t))))
-println("B: ", exp(1/2 * get_B(n,m,t)' * get_A(n,m,t)^-1 * get_B(n,m,t)))
-println("C: ", exp(C(n,m,t)))
-
-int_total(n,m,t)
+t_track = 0.0:0.01:10.0
+ev_11 = [int_total(1,1,t) for t in t_track]
+ev_22 = [int_total(10,10,t) for t in t_track]
+ev_33 = [int_total(100,100,t) for t in t_track]
+# ev_12 = [int_total(1,2,t) for t in t_track]
+# ev_13 = [int_total(1,3,t) for t in t_track]
+# ev_23 = [int_total(2,3,t) for t in t_track]
+B_11 =  [exp(1/2*get_B(1,1,t)' * get_A(1,1,t)^-1 * get_B(1,1,t)) for t in t_track]
+A_11 =  [sqrt((2)^8/det(get_A(1,1,t))) for t in t_track]
+C_11 =  [exp(C(1,1,t)) for t in t_track]
+using Plots
+plt = plot(legend = :outertopleft)
+title!("Integral evolution, g=1e-10")
+xlabel!("Time")
+ylabel!("Integral value")	
+result10 =  [int_total(10,10,t) for t in t_track]
+# plot!(t_track, [imag.(A_11) imag.(B_11) imag.(C_11)], label=["A" "B" "C"], xlabel="Time", ylabel="Imaginary part of the integral")	
+# plot!(t_track, round.([real.(ev_11) real.(ev_22) real.(ev_33)], digits=5), label=["11" "22" "33"], xlabel="Time", ylabel="Real part of the integral")
+plot!(t_track, round.([real.(ev_11) real.(ev_22) real.(ev_33)],digits=10), label=["1" "10" "100"], xlabel="Time", ylabel="Real part of the integral")
+# plot!(t_track, [imag.(ev_11) imag.(ev_22) imag.(ev_33) ], label=["11" "22" "33"], xlabel="Time", ylabel="Imaginary part of the integral")
+# plot!(t_track, [abs.(ev_11),abs.(ev_11),abs.(ev_11)], label=["11" "22" "33"])
+# plot!(t_track, [imag.(ev_12) imag.(ev_13) imag.(ev_23)], label=["12" "13" "23"], xlabel="Time", ylabel="Imaginary part of the integral"
+# plot!(t_track, round.(real.(result10),digits=5), label="10", xlabel="Time", ylabel="Real part of the integral")
+display(plt)
